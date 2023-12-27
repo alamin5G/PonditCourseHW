cb62c2a559c100239db78fbd0ff76f0f8a5aa077
alamin5g <21303134@iubat.edu>
2023-12-27 21:45:12 +0600

    Update the summation using Array

--- src/array_and_function_2nd_class/SumOfArrays.java
+++ src/array_and_function_2nd_class/SumOfArrays.java
@@ -1,2 +1,31 @@
-package array_and_function_2nd_class;public class SumOfArrays {
+package array_and_function_2nd_class;
+
+import java.util.Scanner;
+
+
+public class SumOfArrays {
+
+    /*
+     * 1. Sum of Array Elements:
+     * Write a program to find the sum of all elements in an array.
+     * */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What will be the array size? : ");
+
+        int sizeOfArray = input.nextInt();
+
+        int[] array = new int[sizeOfArray];
+
+        int sum = 0;
+        for(int i=0; i<array.length; i++){
+            System.out.print("Enter the value for elements [" + (i+1) + "] : ");
+            array[i] = input.nextInt();
+            //you can do it in other loop (sum)
+            sum += array[i];
+        }
+
+        System.out.println("Total summation of the array elements is = " + sum);
+
+    }
 }
